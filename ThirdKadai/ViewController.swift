7f36337fd1eda818b6fc7e63b7af2f8413ef2c42
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-09-23 13:58:48 +0900

    進むボタン・戻るボタンの実装

--- ThirdKadai/ViewController.swift
+++ ThirdKadai/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+     let images: [UIImage] = [UIImage(named:"image1.png")!, UIImage(named:"image2.png")!, UIImage(named:"image3.png")!]
+    var imageindex = 0
+    var timer :Timer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = images[imageindex]
     }
+    
+    //戻るボタンを押した時
+    @IBAction func back(_ sender: Any) {
+        if imageindex == 0 {
+            imageindex = 2
+        } else {
+            imageindex -= 1
+        }
+        imageView.image = images[imageindex]
+    }
+    
+    //進むボタンを押した時
+    @IBAction func next(_ sender: Any) {
+        if imageindex == 2 {
+            imageindex = 0
+        } else {
+            imageindex += 1
+        }
+        imageView.image = images[imageindex]
+    }
+    
+    //再生・一時停止を押した時
+    @IBAction func startStop(_ sender: Any) {
+    }
+    
+    //タイマー
 
+    
 
 }
 
