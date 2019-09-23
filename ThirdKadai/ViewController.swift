280ac030183bc1d23acd91e465a1fe53c3ac7c39
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-09-23 13:20:17 +0900

    Initial Commit

---
+++ ThirdKadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdKadai
+//
+//  Created by 鈴木裕奈 on 2019/09/23.
+//  Copyright © 2019 Yuna Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
