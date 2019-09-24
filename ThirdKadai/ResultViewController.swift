a0cbe616e91dcd9a8dc1363414de1860ed67150f
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-09-24 22:13:57 +0900

    画像をタップすると画面遷移＆拡大画像の表示実装

--- ThirdKadai/ResultViewController.swift
+++ ThirdKadai/ResultViewController.swift
@@ -13,13 +13,28 @@ class ResultViewController: UIViewController {
     
     @IBOutlet weak var tappedImageView: UIImageView!
     var selectedImage: UIImage!
-    
+    var screenWidth:CGFloat = 0
+    var screenHeight:CGFloat = 0
+    var width:CGFloat = 0
+    var height:CGFloat = 0
+    var scale:CGFloat = 1.0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.        
+        // Do any additional setup after loading the view.
+        
+        screenWidth = self.view.bounds.width
+        screenHeight = self.view.bounds.height
+        width = selectedImage.size.width
+        height = selectedImage.size.height
+        scale = screenWidth / width
+        tappedImageView.center = CGPoint(x:screenWidth/2, y:screenHeight/2)
+        let rect:CGRect = CGRect(x:0, y:0, width:width*scale, height:height*scale)
+        
+        tappedImageView.frame = rect
         tappedImageView.image = selectedImage
+        
     }
     
 
